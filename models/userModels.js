67dfd8f09055ad28e187b2b40efa56f3faaee269
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-09-26 11:14:06 +0600

    feat: implement user model with schema and timestamps

---
+++ models/userModels.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
